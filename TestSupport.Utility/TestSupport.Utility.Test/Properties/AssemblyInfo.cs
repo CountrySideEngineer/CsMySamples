da73102682d9357ca99bb8012347deded0056aae
CountrySideEngineer <deep-sleeper@nifty.com>
2021-04-30 08:15:52 +0900

    Add project and its test. -Add project to develop utility dynamic link library to support developing test support tools. -Add test project to test the above.

---
+++ TestSupport.Utility/TestSupport.Utility.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSupport.Utility.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSupport.Utility.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5da62b2-c3ca-45ec-b5da-facba35d6bc8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
