6bda782af406b83ba4b3f9b84c5a6c5867daa4de
CountrySideEngineer <deep-sleeper@nifty.com>
2021-07-10 09:35:27 +0900

    Add project for .NETFramework. -Add project to Logger.dll for .NET Framework environment. -Add project to build sample application in the environment. -Add dll binary file build in "Release" condition both .NET Core and framework. -Update solution file.

---
+++ Logger/LoggerSampleDotnetFramework/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using CSEngineer;
+
+namespace LoggerSample
+{
+	/// <summary>
+	/// Sample program using Logger.dll
+	/// </summary>
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<string> logLevels = Logger.AvailableLogLevelList;
+			int index = 0;
+			foreach (var logLevel in logLevels)
+			{
+				Console.WriteLine($"{index} : {logLevel}");
+				index++;
+			}
+
+			Logger.TRACE("This is TRACE level message before initializing stream");
+			Logger.DEBUG("This is DEBUG level message before initializing stream");
+			Logger.INFO("This is INFO level message before initializing stream");
+			Logger.WARN("This is WARN level message before initializing stream");
+			Logger.ERROR("This is ERROR level message before initializing stream");
+			Logger.FATAL("This is FATAL level message before initializing stream");
+
+			using (var logStream = new StreamWriter(@"./log.txt", false, Encoding.UTF8))
+			{
+				Logger.AddStream(logStream);
+				Logger.Level = Logger.LogLevel.All;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.TRACE;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.DEBUG;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.INFO;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.WARN;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.ERROR;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.FATAL;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				Logger.Level = Logger.LogLevel.NONE;
+				Logger.TRACE("This is TRACE level message");
+				Logger.DEBUG("This is DEBUG level message");
+				Logger.INFO("This is INFO level message");
+				Logger.WARN("This is WARN level message");
+				Logger.ERROR("This is ERROR level message");
+				Logger.FATAL("This is FATAL level message");
+
+				bool removeResult = Logger.RemoveStream(logStream);
+				Debug.WriteLine($"Remove result = {removeResult}");
+			}
+
+			Logger.TRACE("This is TRACE level message");
+			return;
+		}
+	}
+}
