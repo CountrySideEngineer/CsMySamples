c5b8069ac303f151da38f79ac94969e0848c14b8
CountrySideEngineer <deep-sleeper@nifty.com>
2021-10-31 09:42:09 +0900

    Add sample about plugin. Add sample code, project, and solution about plugin dynamic loading (when button clicked).

---
+++ DynamicMenuItem/DynamicMenuItem/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using DynamicMenuItem.Model;
+using DynamicMenuItem.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicMenuItem
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Data context changed event handler
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="e">Event args.</param>
+		private void MainWindows_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			DynamicMenuItemViewModel menuItemViewModel = (DynamicMenuItemViewModel)e.NewValue;
+			menuItemViewModel.RaiseNotifyMenuItemEvent += this.NotifyMenuItemEventHandler;
+			menuItemViewModel.RaiseNotifyMenuItemClearEvent += this.NotifyMenuItemClearEventHandler;
+
+			this.RaiseLoadMenuItemRequestEvent += menuItemViewModel.LoadDynamicMenuRequestEventHandler;
+			this.RaiseUnloadMenuItemRequestEvent += menuItemViewModel.UnloadDynamicMenuItemRequestEventHandler;
+		}
+
+		/// <summary>
+		/// Menu item load request event handler
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="args">Event args.</param>
+		public void NotifyMenuItemClearEventHandler(object sender, EventArgs args)
+		{
+			this.UnloadMenuItem();
+		}
+
+		/// <summary>
+		/// Menu item unload event handler.
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="args">Event args.</param>
+		public void NotifyMenuItemEventHandler(object sender, EventArgs args)
+		{
+			NotifyMenuItemEventArgs eventArgs = (NotifyMenuItemEventArgs)args;
+			IEnumerable<string> menuItemTitles = eventArgs.MenuItems;
+			this.LoadMenuItem(menuItemTitles);
+		}
+
+		/// <summary>
+		/// Unload menu items.
+		/// </summary>
+		protected void UnloadMenuItem()
+		{
+			MenuItem rootMenuItem = (MenuItem)this.FindName("MenuRoot");
+			rootMenuItem.Items.Clear();
+		}
+
+		/// <summary>
+		/// Load menu item.
+		/// </summary>
+		/// <param name="menuItemTitles">Collection of menu item title.</param>
+		protected void LoadMenuItem(IEnumerable<string> menuItemTitles)
+		{
+			this.UnloadMenuItem();
+
+			MenuItem rootMenuItem = (MenuItem)this.FindName("MenuRoot");
+			foreach (var menuItemTitle in menuItemTitles)
+			{
+				var menuItem = new MenuItem()
+				{
+					Header = menuItemTitle
+				};
+				rootMenuItem.Items.Add(menuItem);
+			}
+		}
+
+		public delegate void LoadMenuItemRequestEventHandler(object sender, EventArgs args);
+		public event LoadMenuItemRequestEventHandler RaiseLoadMenuItemRequestEvent;
+		public event LoadMenuItemRequestEventHandler RaiseUnloadMenuItemRequestEvent;
+
+		/// <summary>
+		/// Load button click event handler.
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="e">Event args</param>
+		private void LoadButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.RaiseLoadMenuItemRequestEvent?.Invoke(this, new EventArgs());
+
+		}
+
+		/// <summary>
+		/// Unload button clicl event handler.
+		/// </summary>
+		/// <param name="sender">Event sender.</param>
+		/// <param name="e">Event args.</param>
+		private void UnloadButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.RaiseUnloadMenuItemRequestEvent(this, new EventArgs());
+		}
+	}
+}
---
+++ DynamicMenuItem/DynamicMenuItem/Model/EventArgs/NotifyMenuItemEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicMenuItem.Model
+{
+	public class NotifyMenuItemEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Name of menu items.
+		/// </summary>
+		public IEnumerable<string> MenuItems { get; }
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		protected NotifyMenuItemEventArgs()
+		{
+			this.MenuItems = new List<string>();
+		}
+
+		/// <summary>
+		/// Constructor with argument,
+		/// </summary>
+		/// <param name="menuItems">Collection of string to show in menu items.</param>
+		public NotifyMenuItemEventArgs(IEnumerable<string> menuItems)
+		{
+			this.MenuItems = new List<string>(menuItems.ToList());
+		}
+	}
+}
