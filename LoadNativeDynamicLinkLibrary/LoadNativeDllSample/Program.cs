2c1de48d353f4e66a1f5659958ab4aae0b6b591f
CountrySideEngineer <deep-sleeper@nifty.com>
2021-12-19 13:47:08 +0900

    Add project, solution, source code, and binary files. Add sample project, solution, source codes, and binary files (DLL and executable files) about loading DLL in delay.

---
+++ LoadNativeDynamicLinkLibrary/LoadNativeDllSample/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.TeamFoundation.Common.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadNativeDllSample
+{
+	class Program
+	{
+		[DllImport("kernel32.dll")]
+		static extern IntPtr LoadLibrary(string lpFileName);
+		[DllImport("kernel32.dll", CallingConvention=CallingConvention.Cdecl)]
+		static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+		[DllImport("kernel32.dll")]
+		static extern bool FreeLibrary(IntPtr hLibModule);
+
+		delegate int AddDelegate(int value1, int value2);
+		delegate int SumDelegate(IntPtr values, int valueCount);
+
+		static void Main(string[] args)
+		{
+			// 関数ポインタを取得
+			string dllPath = @"E:\development\VisualStudioSample\CsMySamples\LoadNativeDynamicLinkLibrary\bin\x64\Debug\LoadNativeDynamicLinkLibrary.dll";
+
+			IntPtr ptrLib = LoadLibrary(dllPath);
+			IntPtr ptrAdd = GetProcAddress(ptrLib, "Add");
+
+			AddDelegate addDelegate = (AddDelegate)Marshal.GetDelegateForFunctionPointer(ptrAdd, typeof(AddDelegate));
+			int value1 = 10;
+			int value2 = 23;
+			int addedValue = addDelegate(value1, value2);
+			Console.WriteLine($"addedValue = {addedValue}");
+
+			//IntPtr ptrSum = GetProcAddress(ptrLib, "Sum");
+			//SumDelegate sumDelegate = (SumDelegate)Marshal.GetDelegateForFunctionPointer(ptrSum, typeof(SumDelegate));
+			//int valuesCount = 3;
+			//IntPtr values = new IntPtr();
+			//values = Marshal.AllocHGlobal(valuesCount * sizeof(int));
+			//unsafe
+			//{
+			//	int* valuesPtr = (int*)values;
+			//	for (int index = 0; index < valuesCount; index++)
+			//	{
+			//		*(valuesPtr + index) = (index + 1);
+			//	}
+			//}
+			//int sumValue = sumDelegate(values, 3);
+			//Console.WriteLine($"theSum = {sumValue}");
+
+			//Marshal.FreeHGlobal(values);
+			FreeLibrary(ptrLib);
+			ptrLib = IntPtr.Zero;
+
+			//MK2実行
+			dllPath = @"E:\development\VisualStudioSample\CsMySamples\LoadNativeDynamicLinkLibrary\bin\x64\Debug\LoadNativeDynamicLinkLibrary_Mk2.dll";
+			ptrLib = LoadLibrary(dllPath);
+			ptrAdd = GetProcAddress(ptrLib, "Add");
+			addDelegate = (AddDelegate)Marshal.GetDelegateForFunctionPointer(ptrAdd, typeof(AddDelegate));
+			value1 = 21;
+			value2 = 43;
+			addedValue = addDelegate(value1, value2);
+			Console.WriteLine($"addedValue = {addedValue}");
+			FreeLibrary(ptrLib);
+
+			return;
+		}
+	}
+}
---
+++ LoadNativeDynamicLinkLibrary/LoadNativeDynamicLinkLibrary_Mk2/dllFunctions.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include <Windows.h>
+#include <tchar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	/**
+	 * @brief	Sample method to export.
+	 *			This method returns sum of 2 values.
+	 *
+	 * @param	value1	A value to add.
+	 * @param	value2	A value to add.
+	 * @return	Result of the calculation.
+	 */
+	__declspec(dllexport) int Add(int value1, int value2)
+	{
+		_tprintf(_T("LoadNativeDynamicLinkLibrary_Mk2\n"));
+		_tprintf(_T("%s\n"), __FUNCTIONW__);
+		_tprintf(_T("value1 = %d\n"), value1);
+		_tprintf(_T("value2 = %d\n"), value2);
+
+		return (value1 + value2);
+	}
+
+	/**
+	 * @brief	Sample method to export.
+	 *			This method returns sum of values.
+	 *
+	 * @param	Pointer to the collection to add.
+	 * @param	The number of value to add.
+	 * @return	The sum of values.
+	 */
+	__declspec(dllexport) int Sum(int* values, int valueCount)
+	{
+		_tprintf(_T("LoadNativeDynamicLinkLibrary\n"));
+		_tprintf(_T("%s\n"), __FUNCTIONW__);
+
+		LONG	sum = 0;
+		for (int index = 0; index < valueCount; index++)
+		{
+			_tprintf(_T("value%d = %d\n"), (index + 1), *values);
+			sum += *values;
+			values++;
+		}
+		return sum;
+	}
+#ifdef __cplusplus
+}
+#endif
