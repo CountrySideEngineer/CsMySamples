97c23ef03c24e389e5dd076a26c35cba96eea704
CountrySideEngineer <deep-sleeper@nifty.com>
2022-02-02 23:02:05 +0900

    Add library to show progress in a dialog.

---
+++ ProgressWindow/ProgressWindow/Model/Interface/IAsyncTask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountrySideEngineer.ProgressWindow.Model.Interface
+{
+	public interface IAsyncTask<T>
+	{
+		void RunTask(IProgress<T> progress);
+	}
+}
