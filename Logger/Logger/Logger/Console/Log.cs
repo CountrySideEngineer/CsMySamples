9baedc984e0243d77e7f79c69e68903f842d5fc5
CountrySideEngineer <deep-sleeper@nifty.com>
2022-06-30 06:52:13 +0900

    Add class to output log into console.

---
+++ Logger/Logger/Logger/Console/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSEngineer.Logger.Console
+{
+	/// <summary>
+	/// Log class to output message into console, standard output.
+	/// </summary>
+	public class Log : ALog
+	{
+		/// <summary>
+		/// Output log to console.
+		/// </summary>
+		/// <param name="level">Log level in string.</param>
+		/// <param name="message">Log message.</param>
+		public override void Output(string level, string message)
+		{
+			string timeStamp = GetTimeStamp();
+			string log = $"[{level}][{timeStamp}]:{message}";
+
+			System.Console.WriteLine(log);
+		}
+	}
+}
