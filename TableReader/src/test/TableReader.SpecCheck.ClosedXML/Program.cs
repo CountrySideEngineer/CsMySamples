95ece7ffc372ef2c4d37a58ee4c06846fba4237a
CountrySideEngineer <deep-sleeper@nifty.com>
2023-06-09 06:19:22 +0900

    Add project and solution. Add Visual Studio project and solution file to check the time to read a table in excel file using ClosedXML.

---
+++ TableReader/src/test/TableReader.SpecCheck.ClosedXML/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableReader.ClosedXML;
+using TableReader.Interface;
+using System.Data;
+using System.Diagnostics;
+
+namespace TableReader.SpecCheck.ClosedXML
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string testFilePath = @".\..\..\..\TestData\TableReader_SpecCheck.xlsx";
+			long totalTime = 0;
+			for (int index = 0; index < 40; index++)
+			{
+				using (var stream = new FileStream(testFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+				{
+					var stopWatch = new Stopwatch();
+					ITableReader reader = new ExcelTableReader(stream, "Read_test_002");
+					stopWatch.Start();
+					DataTable table = reader.Read("TestTable_001");
+					stopWatch.Stop();
+					Console.WriteLine($"time({index + 1}) = {stopWatch.ElapsedMilliseconds} ms");
+					totalTime += stopWatch.ElapsedMilliseconds;
+				}
+			}
+			Console.Write($"Average = {totalTime / 100} ms");
+			return;
+		}
+	}
+}
