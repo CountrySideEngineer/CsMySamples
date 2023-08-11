c0ad94a4977edd5ec485ce0f254a5991b1a62877
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-11 14:18:29 +0900

    Add project and solution file to develop and check the way to use RichText control in WPF application.

---
+++ RichTextSample/RichTextSample/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RichTextSample
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void richTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				if (!richTextBox.Selection.IsEmpty)
+				{
+					var endPos = richTextBox.Selection.End;
+					var startPos = richTextBox.Selection.Start;
+					var range = new TextRange(startPos, endPos);
+					range.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.DarkBlue);
+				}
+			}
+			catch (Exception) { }
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			RichTextSampleViewModel vm = (RichTextSampleViewModel)DataContext;
+			vm.LoadContent();
+		}
+	}
+}
---
+++ RichTextSample/RichTextSample/RichTextSampleViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CountrySideEngineer.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichTextSample
+{
+	internal class RichTextSampleViewModel : ViewModelBase
+	{
+		protected string _content = string.Empty;
+
+		public string Content
+		{
+			get => _content;
+			set
+			{
+				_content = value;
+				RaisePropertyChanged(nameof(Content));
+			}
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public RichTextSampleViewModel() { }
+
+		public void LoadContent()
+		{
+			string filePath = @"E:\development\googletest_gui\dev\src\bin\Release\netframework\log\SampleTest_NG\output\CAdd_test.calc_001_20230505072007.log";
+			using (var reader = new StreamReader(filePath))
+			{
+				string content = reader.ReadToEnd();
+				Content = content;
+			}
+		}
+
+	}
+}
