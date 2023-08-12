4237ddda01e3b6c17e2f3e92c909eb30ab2a588c
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-13 05:18:57 +0900

    Update path to content to load.

--- RichTextSample/RichTextSample/RichTextSampleViewModel.cs
+++ RichTextSample/RichTextSample/RichTextSampleViewModel.cs
@@ -30,7 +30,7 @@ namespace RichTextSample
 
 		public void LoadContent()
 		{
-			string filePath = @"E:\development\googletest_gui\dev\src\bin\Release\netframework\log\SampleTest_NG\output\CMultiple_test.calc_002_20230505072010.log";
+			string filePath = @".\..\CMultiple_test.calc_002.log";
 			using (var reader = new StreamReader(filePath))
 			{
 				string content = reader.ReadToEnd();
