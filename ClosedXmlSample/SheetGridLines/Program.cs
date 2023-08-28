79066d9fc61ce25fef15dec316606869e750a4b3
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-28 22:02:42 +0900

    Add sample of ClosedXml about sheet grid lines.

---
+++ ClosedXmlSample/SheetGridLines/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheetGridLines
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var gridLines = new SheetGridLines()
+			{
+				Path = @"./ClosedXml_GridLines.xlsx",
+				Sheet = "GridLinesSample"
+			};
+			gridLines.GridLines1();
+		}
+	}
+}
---
+++ ClosedXmlSample/SheetGridLines/SheetGridLines.cs
@@ -0,0 +1,36 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheetGridLines
+{
+	internal class SheetGridLines
+	{
+		public string Path { get; set; }
+
+		public string Sheet { get; set; }
+
+		public SheetGridLines() { }
+
+		public SheetGridLines(string path, string sheet)
+		{
+			Path = path;
+			Sheet = sheet;
+		}
+
+		public void GridLines1()
+		{
+			using (var workbook = new XLWorkbook())
+			{
+				var workSheet = workbook.Worksheets.Add(Sheet);
+				workSheet.ShowGridLines = false;
+				workSheet.Cell(2, 2).Value = "ShowGridLines = false";
+
+				workbook.SaveAs(Path);
+			}
+		}
+	}
+}
