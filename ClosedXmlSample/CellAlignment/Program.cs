09190f6cb0c3a24fd1e19094ecf9ea5b0d593dbd
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-28 21:47:30 +0900

    Add sample to set text alignment.

---
+++ ClosedXmlSample/CellAlignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellAlignment
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var alignment = new CellAlignment()
+			{
+				Path = @"./ClosedXml_alignment1.xlsx",
+				Sheet = "AlignmentSample"
+			};
+			alignment.Alignment1();
+		}
+	}
+}
