7ea84f7a98c21724fcfd0ec06a3327e9d1ca70d2
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-29 22:56:52 +0900

    Add sample to set width of column.

---
+++ ClosedXmlSample/ColumnWidth/ColWidth.cs
@@ -0,0 +1,36 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColumnWidth
+{
+	internal class ColWidth
+	{
+		public string Path { get; set; } = string.Empty;
+
+		public string Sheet { get; set; } = string.Empty;
+
+		public ColWidth() { }
+
+		public ColWidth(string path, string sheet)
+		{
+			Path = path;
+			Sheet = sheet;
+		}
+
+		public void ColumnWidth1()
+		{
+			using (var workbook = new XLWorkbook())
+			{
+				var workSheet = workbook.Worksheets.Add(Sheet);
+				workSheet.Cell(2, 2).Value = $"Column width = 30";
+				workSheet.Column(2).Width = 30;
+
+				workbook.SaveAs(Path);
+			}
+		}
+	}
+}
---
+++ ClosedXmlSample/ColumnWidth/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColumnWidth
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var colWidth = new ColWidth()
+			{
+				Path = @"./ClosedXml_ColWidth.xlsx",
+				Sheet = "ColWidthSample"
+			};
+			colWidth.ColumnWidth1();
+		}
+	}
+}
