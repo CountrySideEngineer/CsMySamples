77b2c93a7258c767a38b572fb2ecc5a1418511d8
CountrySideEngineer <deep-sleeper@nifty.com>
2023-09-30 22:25:19 +0900

    Add sample codes about web api and its client.

---
+++ web/WebApplication1/WebApplication1/Controllers/XmlController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class XmlController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostXmlData([FromBody] Person xmlData)
+        {
+            try
+            {
+                //                var xmlDoc = XDocument.Parse(xmlData);
+                Console.WriteLine($"Persom : Id = {xmlData.Id}, Name = {xmlData.Name}, Age = {xmlData.Age}");
+
+                return Ok("XML data received and processed successfully.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error processing XML data: {ex.Message}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
