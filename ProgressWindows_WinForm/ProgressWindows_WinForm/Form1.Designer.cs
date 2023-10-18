b84834320a4b894ee3581543dcb499bbaab1e72a
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-18 19:19:18 +0900

    Update and add codes to show progress into label control.

--- ProgressWindows_WinForm/ProgressWindows_WinForm/Form1.Designer.cs
+++ ProgressWindows_WinForm/ProgressWindows_WinForm/Form1.Designer.cs
@@ -28,13 +28,97 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.progressTitle = new System.Windows.Forms.Label();
+			this.progressValue = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// backgroundWorker
+			// 
+			this.backgroundWorker.WorkerReportsProgress = true;
+			this.backgroundWorker.WorkerSupportsCancellation = true;
+			this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+			this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+			this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.buttonStart.Location = new System.Drawing.Point(309, 17);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(80, 40);
+			this.buttonStart.TabIndex = 0;
+			this.buttonStart.Text = "START";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Enabled = false;
+			this.buttonCancel.Location = new System.Drawing.Point(309, 63);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(80, 40);
+			this.buttonCancel.TabIndex = 1;
+			this.buttonCancel.Text = "CANCEL";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+			// 
+			// progressTitle
+			// 
+			this.progressTitle.AutoSize = true;
+			this.progressTitle.Font = new System.Drawing.Font("ＭＳ ゴシック", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.progressTitle.Location = new System.Drawing.Point(16, 27);
+			this.progressTitle.Name = "progressTitle";
+			this.progressTitle.Padding = new System.Windows.Forms.Padding(8);
+			this.progressTitle.Size = new System.Drawing.Size(114, 37);
+			this.progressTitle.TabIndex = 2;
+			this.progressTitle.Text = "PROGRESS";
+			this.progressTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// progressValue
+			// 
+			this.progressValue.AutoSize = true;
+			this.progressValue.BackColor = System.Drawing.Color.White;
+			this.progressValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.progressValue.Font = new System.Drawing.Font("ＭＳ ゴシック", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.progressValue.Location = new System.Drawing.Point(141, 17);
+			this.progressValue.Margin = new System.Windows.Forms.Padding(8);
+			this.progressValue.MaximumSize = new System.Drawing.Size(112, 56);
+			this.progressValue.MinimumSize = new System.Drawing.Size(112, 56);
+			this.progressValue.Name = "progressValue";
+			this.progressValue.Padding = new System.Windows.Forms.Padding(16);
+			this.progressValue.Size = new System.Drawing.Size(112, 56);
+			this.progressValue.TabIndex = 3;
+			this.progressValue.Text = "0";
+			this.progressValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(396, 117);
+			this.Controls.Add(this.progressValue);
+			this.Controls.Add(this.progressTitle);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonStart);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "ProgressWindows";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.ComponentModel.BackgroundWorker backgroundWorker;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.Label progressTitle;
+		private System.Windows.Forms.Label progressValue;
 	}
 }
 
--- ProgressWindows_WinForm/ProgressWindows_WinForm/Form1.cs
+++ ProgressWindows_WinForm/ProgressWindows_WinForm/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,66 @@ namespace ProgressWindows_WinForm
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			for (int index = 0;index < 100; index++)
+			{
+				if (true == backgroundWorker.CancellationPending)
+				{
+					e.Cancel = true;
+					break;
+				}
+				else
+				{
+					backgroundWorker.ReportProgress(index + 1);
+					Thread.Sleep(100);
+				}
+			}
+		}
+
+		private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			progressValue.Text = e.ProgressPercentage.ToString() + "%";
+		}
+
+		private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			if (true == e.Cancelled)
+			{
+				progressValue.Text = "canceled";
+			}
+			else if (null != e.Error)
+			{
+				progressValue.Text = "failed";
+			}
+			else
+			{
+				progressValue.Text = "DONE!";
+			}
+
+			buttonStart.Enabled = true;
+			buttonCancel.Enabled = false;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (false == backgroundWorker.IsBusy)
+			{
+				backgroundWorker.RunWorkerAsync();
+				buttonCancel.Enabled = true;
+				buttonStart.Enabled = false;
+			}
+		}
+
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			if (true == backgroundWorker.WorkerSupportsCancellation)
+			{
+				backgroundWorker.CancelAsync();
+			}
+
 		}
 	}
 }
