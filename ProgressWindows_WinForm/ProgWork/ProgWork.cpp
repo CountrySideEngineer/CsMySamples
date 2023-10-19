7eba15718ea5632dbe92619afe358ea3a0224f06
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-19 18:53:46 +0900

    Update the library to make it possible to cancel the background work.

--- ProgressWindows_WinForm/ProgWork/ProgWork.cpp
+++ ProgressWindows_WinForm/ProgWork/ProgWork.cpp
@@ -3,21 +3,30 @@
 #include <tchar.h>
 #include <thread>
 
+static bool isContinueWork = false;
+static bool isRunning = false;
+
 typedef void(__stdcall* ProgressNotification)(long data);
 
 void WINAPI BackgroundWork(ProgressNotification callback)
 {
 	for (long index = 0; index <= 100; index++) {
+		if (!isContinueWork) {
+			break;
+		}
 		if (NULL != callback) {
 			callback(index);
 
-			Sleep(20);
+			Sleep(100);
 		}
 	}
+	isRunning = false;
 }
 
 DWORD WINAPI BackgroundWorkThread(LPVOID parameter)
 {
+	isRunning = true;
+
 	ProgressNotification callback = (ProgressNotification)parameter;
 
 	BackgroundWork(callback);
@@ -29,6 +38,7 @@ void WINAPI BackgroundWorkAsync(ProgressNotification callback)
 {
 	DWORD threadID = 0;
 
+	isContinueWork = true;
 
 	CreateThread(NULL,
 		0,
@@ -38,4 +48,14 @@ void WINAPI BackgroundWorkAsync(ProgressNotification callback)
 		&threadID);
 
 	_tprintf(_T("threadId = %d\n"), threadID);
+}
+
+void WINAPI BackgroundWorkCancle()
+{
+	isContinueWork = false;
+}
+
+bool WINAPI GetBackgroundWorkState()
+{
+	return isRunning;
 }
