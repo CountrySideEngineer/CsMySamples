688603f5c12285d49327317b37cbff668903e1ba
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-29 06:15:44 +0900

    Add controller class. Add controller class about tutorial in the page below: https://learn.microsoft.com/ja-jp/aspnet/core/tutorials/first-mvc-app/adding-controller?view=aspnetcore-7.0&tabs=visual-studio

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /Helloworld/
+        public string Index()
+        {
+            return "This is my deafualt action...";
+        }
+
+        // GET: /HelloWorld/Welcome
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
