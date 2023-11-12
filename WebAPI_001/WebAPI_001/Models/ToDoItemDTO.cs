9dcaf0ccb4f065f9b6f47ede42234f9c2c9ed6ff
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-12 19:17:29 +0900

    Add and update codes in the tutorial project.

---
+++ WebAPI_001/WebAPI_001/Models/ToDoItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_001.Models
+{
+    public class ToDoItemDTO
+    {
+        public long Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
