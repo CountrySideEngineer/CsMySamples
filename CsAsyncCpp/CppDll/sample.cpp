6cabf68f3cc47925ddc0492ac37b89033a49d784
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-18 07:02:32 +0900

    Add sample dll codes in VC++.

---
+++ CsAsyncCpp/CppDll/sample.cpp
@@ -0,0 +1,71 @@
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+
+typedef VOID(*PROGRESS_CALLBACK)(SHORT numerator, SHORT denominator);
+typedef VOID(*RESULT_CALLBACK)(ULONG result);
+
+PROGRESS_CALLBACK	progressCallback = NULL;
+RESULT_CALLBACK		resultCallback = NULL;
+
+VOID
+InitSample()
+{
+	progressCallback = NULL;
+	resultCallback = NULL;
+}
+
+VOID
+WINAPI
+SetProgressCallback(
+	PROGRESS_CALLBACK callback
+)
+{
+	progressCallback = callback;
+}
+
+
+VOID
+WINAPI
+SetResultCallback(
+	RESULT_CALLBACK callback
+)
+{
+	resultCallback = callback;
+}
+
+VOID
+NotifyProgress(
+	SHORT numerator,
+	SHORT denominator
+)
+{
+	if (NULL != progressCallback) {
+		progressCallback(numerator, denominator);
+	}
+}
+
+VOID
+NotifyResult(
+	ULONG result
+)
+{
+	if (NULL != resultCallback) {
+		resultCallback(result);
+	}
+}
+
+#define	PROCESS_MAX			(1234)
+
+VOID
+WINAPI
+Process()
+{
+	for (int index = 0; index < PROCESS_MAX; index++) {
+		Sleep(10);
+
+		NotifyProgress((SHORT)index, (SHORT)PROCESS_MAX);
+	}
+
+	NotifyResult(0);
+}
