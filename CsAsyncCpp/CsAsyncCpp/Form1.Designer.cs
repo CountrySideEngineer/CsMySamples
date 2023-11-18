72a2220a748608cabb9593d089fb0f4a75ae48a3
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-19 05:28:53 +0900

    Update progress and result notification operation.

--- CsAsyncCpp/CsAsyncCpp/Form1.Designer.cs
+++ CsAsyncCpp/CsAsyncCpp/Form1.Designer.cs
@@ -31,17 +31,19 @@
             this.btnExecuteProcess = new System.Windows.Forms.Button();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.progressLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnExecuteProcess
             // 
             this.btnExecuteProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExecuteProcess.Location = new System.Drawing.Point(178, 70);
+            this.btnExecuteProcess.Location = new System.Drawing.Point(178, 117);
             this.btnExecuteProcess.Name = "btnExecuteProcess";
             this.btnExecuteProcess.Size = new System.Drawing.Size(146, 23);
             this.btnExecuteProcess.TabIndex = 0;
-            this.btnExecuteProcess.Text = "button1";
+            this.btnExecuteProcess.Text = "ASYNC";
             this.btnExecuteProcess.UseVisualStyleBackColor = true;
+            this.btnExecuteProcess.Click += new System.EventHandler(this.btnExecuteProcess_Click);
             // 
             // progressBar
             // 
@@ -54,21 +56,38 @@
             // 
             // progressLabel
             // 
-            this.progressLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.progressLabel.AutoSize = true;
             this.progressLabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.progressLabel.Location = new System.Drawing.Point(264, 41);
+            this.progressLabel.Location = new System.Drawing.Point(124, 38);
+            this.progressLabel.MaximumSize = new System.Drawing.Size(200, 24);
+            this.progressLabel.MinimumSize = new System.Drawing.Size(200, 24);
             this.progressLabel.Name = "progressLabel";
-            this.progressLabel.Size = new System.Drawing.Size(60, 21);
+            this.progressLabel.Size = new System.Drawing.Size(200, 24);
             this.progressLabel.TabIndex = 2;
-            this.progressLabel.Text = "label1";
+            this.progressLabel.Text = "100 / 100";
+            this.progressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F);
+            this.resultLabel.Location = new System.Drawing.Point(124, 62);
+            this.resultLabel.MaximumSize = new System.Drawing.Size(200, 24);
+            this.resultLabel.MinimumSize = new System.Drawing.Size(200, 24);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(200, 24);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "label1";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 105);
+            this.ClientSize = new System.Drawing.Size(336, 152);
+            this.Controls.Add(this.resultLabel);
             this.Controls.Add(this.progressLabel);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.btnExecuteProcess);
@@ -86,6 +105,7 @@
         private System.Windows.Forms.Button btnExecuteProcess;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
 
--- CsAsyncCpp/CsAsyncCpp/Form1.cs
+++ CsAsyncCpp/CsAsyncCpp/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,39 @@ namespace CsAsyncCpp
 {
     public partial class Form1 : Form
     {
+        Worker worker;
+
+        IProgress<ProgressInfo> _progress;
+        IProgress<int> _notifyResult;
+
         public Form1()
         {
             InitializeComponent();
+
+            _progress = new Progress<ProgressInfo>(e =>
+            {
+                if (0 != e.Denominator)
+                {
+                    progressLabel.Text = $"{e.Numerator} / {e.Denominator}";
+                    progressBar.Value = (100 * e.Numerator / e.Denominator);
+                }
+            });
+            _notifyResult = new Progress<int>(e =>
+            {
+                resultLabel.Text = $"ResultCode : {e:d5}";
+            });
+            worker = new Worker
+            {
+                Progress = _progress,
+                Result = _notifyResult
+            };
+
+            resultLabel.Text = string.Empty;
+        }
+
+        private void btnExecuteProcess_Click(object sender, EventArgs e)
+        {
+            _ = worker.RunAsync();
         }
     }
 }
