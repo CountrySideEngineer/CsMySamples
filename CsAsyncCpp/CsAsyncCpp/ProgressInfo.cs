06870553db4e93a869c49ec3edbaa3a4ee8516e2
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-19 06:23:12 +0900

    Add codes. Add ProgressInfo class, to which contains the progress information. Add Worker class. to which call cpp library functions to run a process in sub thread.

---
+++ CsAsyncCpp/CsAsyncCpp/ProgressInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsAsyncCpp
+{
+    internal class ProgressInfo
+    {
+        public short Numerator { get; set; } = 0;
+        public short Denominator { get; set; } = 0;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ProgressInfo() { }
+    }
+}
---
+++ CsAsyncCpp/CsAsyncCpp/Worker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsAsyncCpp
+{
+    internal class Worker
+    {
+        protected static Worker _worker = null;
+        public static void OnProgressChanged(short numerator, short denominator)
+        {
+            _worker?.Progress?.Report(new ProgressInfo()
+            {
+                Numerator = (short)numerator,
+                Denominator = (short)denominator
+            });
+        }
+
+        public static void OnResultNotification(ulong result)
+        {
+            _worker?.Result?.Report((short)result);
+        }
+
+        public static Worker GetInstance()
+        {
+            if (null == _worker)
+            {
+                _worker = new Worker();
+            }
+            return _worker;
+        }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        protected Worker() { }
+
+        public IProgress<ProgressInfo> Progress { get; set; }
+        public IProgress<int> Result { get; set; }
+
+        public async Task RunAsync()
+        {
+            await Task.Run(() =>
+            {
+                CppDll.SetResultCallback(OnResultNotification);
+                CppDll.SetProgressCallback(OnProgressChanged);
+                CppDll.Process();
+            });
+        }
+    }
+}
