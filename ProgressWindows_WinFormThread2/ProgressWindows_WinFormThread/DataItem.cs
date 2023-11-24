08b3780e23ffae9683c5bb62cbc0a0b306f909d3
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-25 06:59:08 +0900

    Add DataItem class. Add DataItem class which keeps parameters to be displayed in DataGridView.

---
+++ ProgressWindows_WinFormThread2/ProgressWindows_WinFormThread/DataItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressWindows_WinFormThread2
+{
+    internal class DataItem
+    {
+        public bool IsChecked { get; set; }
+
+        public string Name { get; set; }
+
+        public short Progress { get; set; }
+
+        public ulong Result { get; set; }
+
+        public string ResultCode {
+            get
+            {
+                string _resultCode = $"0x{Result:X8}";
+                return _resultCode;
+            }
+        }
+    }
+}
