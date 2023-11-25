6d5bdd711170fe13d9c05b07e0b1c11216b4c482
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-25 18:32:33 +0900

    Add ProgressWorkDll class. Add a class as interface to access dll API function from C# area.

---
+++ ProgressWindows_WinFormThread2/ProgressWindows_WinFormThread/ProgressWorkDll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressWindows_WinFormThread2
+{
+    internal static class ProgressWorkDll
+    {
+        [DllImport("ProgressWork.dll")]
+        public static extern void InitProgress();
+
+        [DllImport("ProgressWork.dll")]
+        public static extern void GetProgresses(
+            ref short progresses, 
+            int progressNum, 
+            ref int actProgressNum);
+
+        [DllImport("ProgressWork.dll")]
+        public static extern void RunProgress(short interval);
+    }
+}
