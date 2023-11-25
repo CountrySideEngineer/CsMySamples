f61bccf8b08cf28cc2b8a249016653a917a63bf5
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-25 17:54:26 +0900

    Add a code to run in background.

---
+++ ProgressWindows_WinFormThread2/ProgressWork/ProgressWork.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+
+#define	PROGRESS_SIZE		(10)
+
+SHORT	progress[PROGRESS_SIZE];
+
+VOID
+WINAPI
+InitProgress()
+{
+	for (int index = 0; index < PROGRESS_SIZE; index++) {
+		progress[index] = 0;
+	}
+}
+
+VOID
+WINAPI
+GetProgresses(
+	SHORT*	progresses,
+	LONG	progressNum,
+	LONG*	actProgressNum
+)
+{
+	LONG	numToSet = progressNum;
+	if (PROGRESS_SIZE < progressNum) {
+		progressNum = PROGRESS_SIZE;
+	}
+
+	for (LONG index = 0; index < progressNum; index++) {
+		progresses[index] = progress[index];
+	}
+
+	*actProgressNum = progressNum;
+}
+
+VOID
+WINAPI
+RunProgress(SHORT interval)
+{
+	for (int index1 = 0; index1 < PROGRESS_SIZE; index1++) {
+		for (int index2 = 0; index2 < 100; index2++) {
+			progress[index1] = index2;
+			Sleep(interval);
+		}
+	}
+}
