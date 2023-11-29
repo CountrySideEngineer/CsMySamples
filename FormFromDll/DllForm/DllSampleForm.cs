534c39ec3df262a531f84ae051949209f179bc77
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-29 20:58:17 +0900

    Add VisualStudio project to show a form in DLL.

---
+++ FormFromDll/DllForm/DllSampleForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DllForm
+{
+    public partial class DllSampleForm : Form
+    {
+        public DllSampleForm()
+        {
+            InitializeComponent();
+
+            this.Text = "HELLO, DIALOG";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "This is sample form in DLL.",
+                "SAMPLE FORM",
+                MessageBoxButtons.OK);
+        }
+    }
+}
