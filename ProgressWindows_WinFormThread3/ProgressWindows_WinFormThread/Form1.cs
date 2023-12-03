7ea0b596fb7a6033aa0a1bcdfcbe7e7201a9e87e
CountrySideEngineer <deep-sleeper@nifty.com>
2023-12-03 21:36:17 +0900

    Add classes to update view design and action in the form.

--- ProgressWindows_WinFormThread3/ProgressWindows_WinFormThread/Form1.cs
+++ ProgressWindows_WinFormThread3/ProgressWindows_WinFormThread/Form1.cs
@@ -12,9 +12,56 @@ namespace ProgressWindows_WinFormThread
 {
     public partial class Form1 : Form
     {
+        internal Worker _worker = new Worker();
         public Form1()
         {
             InitializeComponent();
+
+            SetupDataBinding();
+            dataGridView1.DataSource = bindingSource;
+        }
+
+        public void OnWorkTaskFinished(object sender, EventArgs e)
+        {
+            _ = _worker.GetProgressAsync(4);
+            startButton.Enabled = true;
+        }
+
+        public void OnProgressGet(object sender, EventArgs e)
+        {
+            var eventArg = e as ProgressGetEventArgs;
+            var progress = eventArg.Progress;
+            var items = progress.ToList();
+
+            bindingSource.DataSource = progress;
+            bindingSource.ResetBindings(false);
+        }
+
+        public void SetupDataBinding()
+        {
+            var dataItems = DataItem.Factory().ToList();
+
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = dataItems;
+            dataGridView1.DataSource = bindingSource;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            progressCheckTimer.Start();
+            startButton.Enabled = false;
+        }
+
+        private void SetupTimer()
+        {
+            progressCheckTimer.Tick += (s, arg) =>
+            {
+                _ = _worker.GetProgressAsync(4);
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
---
+++ ProgressWindows_WinFormThread3/ProgressWindows_WinFormThread/Worker.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressWindows_WinFormThread
+{
+    internal class Worker
+    {
+        public delegate void WorkTaskFinishedEventHandler(object sender, EventArgs e);
+        public event WorkTaskFinishedEventHandler WorkTaskFinished;
+
+        public delegate void ProgressGetEventHandler(object sender, EventArgs e);
+        public event ProgressGetEventHandler ProgressGet;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Worker() { }
+
+        public async Task RunAsync(IEnumerable<DataItem> items)
+        {
+            await Task.Run(() =>
+            {
+                int count = items.Count();
+                DataItemTag[] itemTags = new DataItemTag[count];
+                for (int index = 0; index < count; index++)
+                {
+                    itemTags[index] = items.ElementAt(index).ToStruct();
+                }
+
+                InitProgress();
+
+                ProgressWorkDll.RunProgress(20, itemTags, count);
+            });
+
+            WorkTaskFinished?.Invoke(this, null);
+        }
+
+        public void InitProgress()
+        {
+            ProgressWorkDll.InitProgress();
+        }
+
+        public virtual async Task GetProgressAsync(int count)
+        {
+            short[] progress = null;
+            await Task.Run(() =>
+            {
+                GetProgress(count, out progress);
+            });
+            var eventArg = new ProgressGetEventArgs()
+            {
+                Progress = progress
+            };
+            ProgressGet?.Invoke(this, eventArg);
+        }
+
+        public void GetProgress(int count, out short[] progressData)
+        {
+            progressData = new short[count];
+            int actCount = -1;
+
+            ProgressWorkDll.GetProgresses(progressData, count, ref actCount);
+        }
+
+        public IEnumerable<DataItem> GetInformation(IEnumerable<DataItem> items)
+        {
+            int actCount = -1;
+            int count = items.Count();
+            short[] progress = new short[count];
+            short[] result = new short[count];
+
+            ProgressWorkDll.GetProgresses(progress, count, ref actCount);
+            ProgressWorkDll.GetResult(result, count, ref actCount);
+
+            for (int index = 0; index < actCount; index++)
+            {
+                var progItem = progress[index];
+                var resultItem = result[index];
+
+                var dataItem = new DataItem()
+                {
+                    IsChecked = items.ElementAt(index).IsChecked,
+                    Name = items.ElementAt(index).Name,
+                    Progress = progItem,
+                    Result = Convert.ToUInt64(resultItem)
+                };
+                yield return dataItem;
+            }
+        }
+    }
+}
