942e2b763a298f7c04cb5ce72bbec59c9c3d47ac
CountrySideEngineer <deep-sleeper@nifty.com>
2023-12-16 07:05:06 +0900

    Add solution, project, and source code files about multi language.

---
+++ WinFormMultiLanguage_2/WinFormMultiLanguage/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using WinFormMultiLanguage.Properties;
+
+namespace WinFormMultiLanguage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pushMeButton_Click(object sender, EventArgs e)
+        {
+            string message = Properties.Resources.ResourceManager.GetString("ID_MESSAGE_BOX_MESSAGE");
+            string title = Properties.Resources.ResourceManager.GetString("ID_MESSAGE_BOX_TITLE");
+
+            MessageBox.Show(message, title, MessageBoxButtons.OK);
+        }
+    }
+}
