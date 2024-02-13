27ac4bc123da4872c81b0171fef0e2e041695098
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-14 05:50:43 +0900

    Add new sample project and codes about system event log. Add sample project and codes to delete event source name data when it is registered.

---
+++ SystemLogSample/SystemLogSample_003/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemLogSample_003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceName = "SystemLogSample";
+
+            if (System.Diagnostics.EventLog.SourceExists(sourceName))
+            {
+                System.Diagnostics.EventLog.DeleteEventSource(sourceName);
+            }
+        }
+    }
+}
