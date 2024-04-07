c8b165a9a1bfe20ae999dc5a0c346c1fe82fbade
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-07 19:43:45 +0900

    Add sample about value and reference. Add VisualStudio sample solution and project about value and reference pass of argument to method.

---
+++ ValueAndRefType/ValueAndRefType_002/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndRefType_002
+{
+	public class SampleClass
+	{
+		public SampleClass()
+		{
+			SampleValue = 0;
+		}
+
+		public int SampleValue { get; set; }
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var sampleClass = new SampleClass()
+			{
+				SampleValue = 20
+			};
+			Console.WriteLine($"In Main : SampleValue = {sampleClass.SampleValue} ");
+
+			SampleMethod(sampleClass);
+
+			Console.WriteLine($"In Main : SampleValue = {sampleClass.SampleValue} ");
+		}
+
+		static void SampleMethod(SampleClass sampleClass)
+		{
+			sampleClass.SampleValue = 22;
+
+			Console.WriteLine($"In Sub  : SampleValue = {sampleClass.SampleValue} ");
+		}
+	}
+}
