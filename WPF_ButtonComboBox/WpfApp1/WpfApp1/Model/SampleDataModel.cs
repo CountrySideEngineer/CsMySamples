d9e40e5160f84bc737af084173b10fb083772d93
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-11 22:35:21 +0900

    Add SampleDataModel class.

---
+++ WPF_ButtonComboBox/WpfApp1/WpfApp1/Model/SampleDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.Model
+{
+	internal class SampleDataModel
+	{
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		public SampleDataModel()
+		{
+			Title = string.Empty;
+			Content = string.Empty;
+		}
+	}
+}
