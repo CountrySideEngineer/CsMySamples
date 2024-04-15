401cee71be5228e556ecfb96cb985eddac30e6c6
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-15 21:14:31 +0900

    Update SampleDataModel class. Update the class by adding a property collection of SampleDataItemModel as ObservableCollection data type, named Items2.

--- WPF_ButtonComboBox/WpfApp1/WpfApp1/Model/SampleDataModel.cs
+++ WPF_ButtonComboBox/WpfApp1/WpfApp1/Model/SampleDataModel.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace WpfApp1.Model
+namespace WpfApp1.Model 
 {
-	internal class SampleDataModel
+	internal class SampleDataModel 
 	{
 		public string Title { get; set; }
 
 		public string Content { get; set; }
 
+		public ObservableCollection<string> Items { get; set; }
+
+		public ObservableCollection<SampleDataItemModel> Items2 { get; set; }
+
+		public string Item { get; set; }
+
 		public SampleDataModel()
 		{
 			Title = string.Empty;
 			Content = string.Empty;
+			Items = new ObservableCollection<string>();
+			Item = string.Empty;
+			Items2 = new ObservableCollection<SampleDataItemModel>()
+			{
+				new SampleDataItemModel()
+				{
+					Summary = "Summary_001",
+					Detail = "Detail_001"
+				}
+			};
 		}
 	}
 }
