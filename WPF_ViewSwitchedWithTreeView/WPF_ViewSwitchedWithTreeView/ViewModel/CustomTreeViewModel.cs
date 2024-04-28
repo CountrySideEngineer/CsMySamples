e8629120ab505ce855257ce410d7a61d9b64dc43
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-28 23:01:38 +0900

    Add new view and view model classes.

---
+++ WPF_ViewSwitchedWithTreeView/WPF_ViewSwitchedWithTreeView/ViewModel/CustomTreeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ViewSwitchedWithTreeView.ViewModel
+{
+	internal class CustomTreeViewModel : ViewModelBase
+	{
+		protected IEnumerable<string> _treeItems;
+		public IEnumerable<string> TreeItems
+		{
+			get => _treeItems;
+			set
+			{
+				_treeItems = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		public CustomTreeViewModel()
+		{
+			TreeItems = new List<string>()
+			{
+				"TreeItem1",
+				"TreeItem2",
+				"TreeItem3",
+				"TreeItem4",
+			};
+		}
+	}
+}
