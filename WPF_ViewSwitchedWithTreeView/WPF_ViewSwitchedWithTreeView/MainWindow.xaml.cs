610fe42b4ed0158f353b4387fa2235a625c713d9
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-29 15:16:06 +0900

    Update MainWindow view. Add ContentPresenter control to show a view, which inherits UserControl class.

--- WPF_ViewSwitchedWithTreeView/WPF_ViewSwitchedWithTreeView/MainWindow.xaml.cs
+++ WPF_ViewSwitchedWithTreeView/WPF_ViewSwitchedWithTreeView/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_ViewSwitchedWithTreeView.View;
 
 namespace WPF_ViewSwitchedWithTreeView
 {
@@ -20,9 +23,58 @@ namespace WPF_ViewSwitchedWithTreeView
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		protected Dictionary<string, string> _ViewPathDictionary = new Dictionary<string, string>
+		{
+			{ "TreeItem1", "WPF_ViewSwitchedWithTreeView.View.CustomUserViewControl_001" },
+			{ "TreeItem2", "WPF_ViewSwitchedWithTreeView.View.CustomUserViewControl_002" },
+			{ "TreeItem3", "WPF_ViewSwitchedWithTreeView.View.CustomUserViewControl_003" },
+			{ "TreeItem4", "WPF_ViewSwitchedWithTreeView.View.CustomUserViewControl_004" },
+		};
+
+		protected Dictionary<string, UIElement> _ViewDictionary = new Dictionary<string, UIElement>();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			this.CustomTreeView.SelectedItemChangedEventHandler += CustomTreeView_SelectedItemChangedEventHandler;
+		}
+
+		protected virtual void CustomTreeView_SelectedItemChangedEventHandler(object sender, EventArgs e)
+		{
+			var selectedItemInfo = (RoutedPropertyChangedEventArgs<object>)e;
+
+			string selectedItemName = (string)selectedItemInfo.NewValue;
+
+			var element = GetSelectedPage(selectedItemName);
+			ContentPresenter.Content = element;
+		}
+
+		protected virtual UIElement GetSelectedPage(string selectedName)
+		{
+			try
+			{
+				var element = _ViewDictionary[selectedName];
+				return element;
+			}
+			catch (KeyNotFoundException)
+			{
+				var path = GetSelectedPath(selectedName);
+				var type = Type.GetType(path);
+
+				var element = (UIElement)Activator.CreateInstance(type);
+
+				_ViewDictionary.Add(selectedName, element);
+
+				return element;
+			}
+		}
+
+		protected virtual string GetSelectedPath(string selectedName)
+		{
+			string path = _ViewPathDictionary[selectedName];
+
+			return path;
 		}
 	}
 }
