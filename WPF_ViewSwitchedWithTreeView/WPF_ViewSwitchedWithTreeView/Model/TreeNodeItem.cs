f2472b9be704013eb752389bd151ecf992f6157a
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-29 17:19:18 +0900

    Update TreeNodeItem class by adding new properties.

--- WPF_ViewSwitchedWithTreeView/WPF_ViewSwitchedWithTreeView/Model/TreeNodeItem.cs
+++ WPF_ViewSwitchedWithTreeView/WPF_ViewSwitchedWithTreeView/Model/TreeNodeItem.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace WPF_ViewSwitchedWithTreeView.Model
 {
 	internal class TreeNodeItem
 	{
-		public string Name { get; set; }
+		public string Name { get; set; } = string.Empty;
 
-		public object ViewModel { get; set; }
+		public string Path { get; set; } = string.Empty;
 
-		public TreeNodeItem()
-		{
-			Name = string.Empty;
-			ViewModel = null;
-		}
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TreeNodeItem() { }
 	}
 }
