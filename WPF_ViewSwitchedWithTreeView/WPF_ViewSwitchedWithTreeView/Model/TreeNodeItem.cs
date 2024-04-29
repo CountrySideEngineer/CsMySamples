82d8b4d085b79b6fbdc9da798278a0a16c09f934
CountrySideEngineer <deep-sleeper@nifty.com>
2024-04-29 14:57:24 +0900

    Add a new model class.

---
+++ WPF_ViewSwitchedWithTreeView/WPF_ViewSwitchedWithTreeView/Model/TreeNodeItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ViewSwitchedWithTreeView.Model
+{
+	internal class TreeNodeItem
+	{
+		public string Name { get; set; }
+
+		public object ViewModel { get; set; }
+
+		public TreeNodeItem()
+		{
+			Name = string.Empty;
+			ViewModel = null;
+		}
+	}
+}
