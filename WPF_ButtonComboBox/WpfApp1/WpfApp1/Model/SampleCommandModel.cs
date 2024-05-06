ad15b6147cee0aab24bffaef97a9696d77b5b0d5
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-06 10:05:06 +0900

    Add new command class. SampleCommandModel class, a command to be executed when an item of combo box is selected.

---
+++ WPF_ButtonComboBox/WpfApp1/WpfApp1/Model/SampleCommandModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+	public class SampleCommandModel
+	{
+		public string Title { get; set; } = string.Empty;
+
+		public Func<string> Command { get; set; } = null;
+	}
+}
