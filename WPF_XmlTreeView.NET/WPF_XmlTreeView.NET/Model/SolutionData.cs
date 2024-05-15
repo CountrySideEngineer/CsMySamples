9ed0e074f51e3ce1dc3d6e32c753a1f06133bff0
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-16 08:06:11 +0900

    Add view model and model classes as sample.

---
+++ WPF_XmlTreeView.NET/WPF_XmlTreeView.NET/Model/SolutionData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml.Serialization;
+
+namespace WPF_XmlTreeView.NET.Model
+{
+	[XmlRoot("TestSolution")]
+	internal class SolutionData
+	{
+		[XmlAttribute(nameof(Name))]
+		public string Name { get; set; } = "";
+
+		[XmlElement("TestProject")]
+		public IEnumerable<ProjectItem>? Items { get; set; } = null;
+	}
+
+	internal class ProjectItem
+	{
+		[XmlAttribute(nameof(Name))]
+		public string Name { get; set; } = string.Empty;
+
+		[XmlAttribute(nameof(Path))]
+		public string Path { get; set; } = string.Empty;
+
+		[XmlElement("Item")]
+		public IEnumerable<ProjectItem>? Items { get; set; } = null;
+	}
+}
---
+++ WPF_XmlTreeView.NET/WPF_XmlTreeView.NET/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_XmlTreeView.NET.Model;
+
+namespace WPF_XmlTreeView.NET.ViewModel
+{
+	internal class MainWindowViewModel : ViewModelBase
+	{
+		public ObservableCollection<SolutionData> Solutions
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public MainWindowViewModel() {
+			Solutions = new ObservableCollection<SolutionData>()
+			{
+				new SolutionData
+				{
+					Name = "Sample solution",
+					Items = new List<ProjectItem>()
+					{
+						new ProjectItem()
+						{
+							Name ="Name_001",
+							Path ="Path_001",
+							Items = new List<ProjectItem>()
+							{
+								new ProjectItem()
+								{
+									Name = "Name_001_001",
+								},
+								new ProjectItem()
+								{
+									Name = "Name_001_002",
+								},
+							}
+						},
+						new ProjectItem()
+						{
+							Name ="Name_002",
+							Path ="Path_002",
+							Items = new List<ProjectItem>()
+							{
+								new ProjectItem()
+								{
+									Name = "Name_002_001",
+								},
+								new ProjectItem()
+								{
+									Name = "Name_002_002",
+								},
+								new ProjectItem()
+								{
+									Name = "Name_002_003",
+								},
+							}
+						},
+						new ProjectItem()
+						{
+							Name ="Name_003",
+							Path ="Path_003",
+						},
+					}
+				}
+			};
+		}
+
+	}
+}
