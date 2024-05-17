c4ceeac018f8944bdf575cd11d12225369d41195
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-18 07:47:24 +0900

    Add view model classes. View model classes for custom user control are added.

---
+++ WPF_XmlTreeView.NET/WPF_XmlTreeView.NET/ViewModel/CustomControlViewModel_001.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_XmlTreeView.NET.Command;
+
+namespace WPF_XmlTreeView.NET.ViewModel
+{
+	internal class CustomControlViewModel_001 : ViewModelBase
+	{
+		protected DelegateCommand? _customCommand = null;
+		public DelegateCommand? CustomCommand
+		{
+			get
+			{
+				if (null == _customCommand)
+				{
+
+				}
+				return _customCommand;
+			}
+		}
+
+		protected string _content_001 = string.Empty;
+		public string Content_001
+		{
+			get => _content_001;
+			set
+			{
+				_content_001 = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		protected string _content_002 = string.Empty;
+		public string Content_002
+		{
+			get => _content_002;
+			set
+			{
+				_content_002 = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		public virtual void CustomCommandExecute()
+		{
+			Console.WriteLine($"{nameof(CustomCommandExecute)} called.");
+		}
+
+
+
+	}
+}
