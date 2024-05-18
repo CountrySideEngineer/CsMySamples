c48c55fd8233a8cfc5af8fb5467b80186895d287
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-18 10:18:43 +0900

    Add a selector class. ControlContentSelector is added.

---
+++ WPF_XmlTreeView.NET/WPF_XmlTreeView.NET/Selector/ControlContentSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WPF_XmlTreeView.NET.ViewModel;
+
+namespace WPF_XmlTreeView.NET.Selector
+{
+	internal class ControlContentSelector : DataTemplateSelector
+	{
+		public override DataTemplate? SelectTemplate(object item, DependencyObject container)
+		{
+			var element = container as FrameworkElement;
+			var viewModel = item as MainWindowViewModel;
+			if (element == null || viewModel == null)
+			{
+				return null;
+			}
+
+			string templateType = "";
+			switch (viewModel.SelectedItem)
+			{
+				default:
+					templateType = "";
+					break;
+			}
+
+			return templateType == "" ? null : element.FindResource(templateType) as DataTemplate;
+		}
+	}
+}
