c3c81a96c7032c13abf5ae94679a01162155ad8f
CountrySideEngineer <deep-sleeper@nifty.com>
2024-05-21 22:40:06 +0900

    Add a new model class into SolutionData.

--- WPF_XmlTreeView.NET/WPF_XmlTreeView.NET/Model/SolutionData.cs
+++ WPF_XmlTreeView.NET/WPF_XmlTreeView.NET/Model/SolutionData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Accessibility;
+using System;
 using System.Collections.Generic;
 using System.IO.Packaging;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Xml.Serialization;
+using WPF_XmlTreeView.NET.ViewModel;
 
 namespace WPF_XmlTreeView.NET.Model
 {
@@ -19,18 +21,73 @@ namespace WPF_XmlTreeView.NET.Model
 		public IEnumerable<ProjectItem>? Items { get; set; } = null;
 	}
 
-	internal class ProjectItem
+	internal class ProjectItem : ViewModelBase
 	{
+		protected string _name = string.Empty;
+
 		[XmlAttribute(nameof(Name))]
-		public string Name { get; set; } = string.Empty;
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				_name = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		protected string _path = string.Empty;
 
 		[XmlAttribute(nameof(Path))]
-		public string Path { get; set; } = string.Empty;
+		public string Path
+		{
+			get => _path;
+			set
+			{
+				_path = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		protected IEnumerable<ProjectItem>? _items = null;
 
 		[XmlElement("Item")]
-		public IEnumerable<ProjectItem>? Items { get; set; } = null;
+		public IEnumerable<ProjectItem>? Items
+		{
+			get => _items;
+			set
+			{
+				_items = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		protected string _typeName = string.Empty;
 
 		[XmlAttribute(nameof(TypeName))]
-		public string? TypeName { get; set; } = string.Empty;
+		public string? TypeName
+		{
+			get => _typeName;
+			set
+			{
+				_typeName = value;
+				RaisePropertyChanged();
+			}
+		}
+	}
+
+	internal class ProjectItemEx : ProjectItem
+	{
+		protected string _exName = string.Empty;
+
+		public string ExName
+		{
+			get => _exName;
+			set
+			{
+				_exName = value;
+				RaisePropertyChanged();
+			}
+		}
 	}
 }
