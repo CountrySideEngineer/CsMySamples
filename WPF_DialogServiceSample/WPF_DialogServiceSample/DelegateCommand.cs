11b91896ead0319f01b116c5165362892ed1e3ca
CountrySideEngineer <deep-sleeper@nifty.com>
2025-05-03 18:07:58 +0900

    Add sample solution and project to show a dialog via view model. Add C# sample application to display a dialog, open file dialog, via view model, and set content into the control on the window.

---
+++ WPF_DialogServiceSample/WPF_DialogServiceSample/DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_DialogServiceSample
+{
+    internal class DelegateCommand : ICommand
+    {
+        protected Action? _execute;
+        protected Func<bool> _canExecute;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute?.Invoke();
+        }
+
+        public DelegateCommand(Action? execute)
+        {
+            _execute = execute;
+            _canExecute = () => { return true; };
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+    }
+}
