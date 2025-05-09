e5d4f2beff61a0bf67cfaadfa62ec92b6bf30171
CountrySideEngineer <deep-sleeper@nifty.com>
2025-05-10 07:15:23 +0900

    Add SampleModel class.

---
+++ WPF_MultiWindow/src/WPF_MultiWindow/Model/SampleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MultiWindow.Model
+{
+    internal class SampleModel
+    {
+        protected ulong _sampleValue = 0;
+
+        public SampleModel() { }
+
+        public SampleModel(ulong sampleValue)
+        {
+            _sampleValue = sampleValue;
+        }
+
+        public ulong UpdateValue()
+        {
+            _sampleValue++;
+
+            return _sampleValue;
+        }
+    }
+}
