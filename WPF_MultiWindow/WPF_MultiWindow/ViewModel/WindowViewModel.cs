3c806153294c4e05727108bcd51196d517db4c1c
CountrySideEngineer <deep-sleeper@nifty.com>
2025-05-09 21:36:52 +0900

    Add WindowViewModel class.

---
+++ WPF_MultiWindow/WPF_MultiWindow/ViewModel/WindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MultiWindow.Command;
+
+namespace WPF_MultiWindow.ViewModel
+{
+    internal class WindowViewModel : ViewModelBase
+    {
+        public delegate void NotifyClosingWindow(object sender, EventArgs e);
+        public NotifyClosingWindow? NotifyClosingWindowDelegate;
+
+        public delegate void CloseWindowDelegate(object sender, EventArgs e);
+        public event CloseWindowDelegate? CloseWindowEvent;
+
+
+        protected DelegateCommand? _closeWindowCommand = null;
+        public DelegateCommand CloseWindowCommand
+        {
+            get
+            {
+                if (null == _closeWindowCommand)
+                {
+                    _closeWindowCommand = new DelegateCommand(CloseWindowCommandExecute);
+
+                }
+                return _closeWindowCommand;
+            }
+        }
+
+        public virtual void OnClosingWindowEventHandler(object sender, EventArgs e)
+        {
+            NotifyClosingWindowDelegate?.Invoke(this, e);
+        }
+
+        protected virtual void RaiseCloseWindowEvent()
+        {
+            CloseWindowEvent?.Invoke(this, new EventArgs());
+        }
+
+        public virtual void CloseWindowCommandExecute()
+        {
+            RaiseCloseWindowEvent();
+        }
+
+        public virtual void ClosingWindowCommandExecute()
+        {
+            RaiseCloseWindowEvent();
+        }
+    }
+}
