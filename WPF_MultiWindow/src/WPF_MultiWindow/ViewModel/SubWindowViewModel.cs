ca2f603f010c5953b954338ec5a34038e35bcc82
CountrySideEngineer <deep-sleeper@nifty.com>
2025-05-10 07:25:00 +0900

    Update SubWindow and its ViewModel class. -Update TextBox by adding binding to "SampleContent" in the view model. -Implement SubWindowViewModel.

--- WPF_MultiWindow/src/WPF_MultiWindow/ViewModel/SubWindowViewModel.cs
+++ WPF_MultiWindow/src/WPF_MultiWindow/ViewModel/SubWindowViewModel.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
+using WPF_MultiWindow.Model;
 
 namespace WPF_MultiWindow.ViewModel
 {
     internal class SubWindowViewModel : WindowViewModel
     {
+        protected ulong _sampleContent = 0;
+        public ulong SampleContent
+        {
+            get => _sampleContent;
+            set
+            {
+                _sampleContent = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        protected DispatcherTimer _timer;
+
+        public SampleModel SampleModel = new SampleModel();
+
+        public SubWindowViewModel() : base()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(250);
+            _timer.Tick += (s, e) => UpdateContent();
+            _timer.Start();
+        }
+
+        public void UpdateContent()
+        {
+            ulong content = SampleModel.UpdateValue();
+
+            SampleContent = content;
+        }
     }
 }
